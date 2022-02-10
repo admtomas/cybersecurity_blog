e26e2713d10bb66833d6461549fb799b5a5b3a9c
admtomas <admtomas@gmail.com>
2022-02-10 16:32:04 +0000

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor_uploader.fields import RichTextUploadingField
+
+#* --- Create model Manager ---
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+#* --- Create Model for The Blog Post ---
+class Post(models.Model):
+    STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to='images/%d/%m/%Y/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
+    # Attach text editor to the body to style the article
+    body=RichTextUploadingField()
+
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+        def __str__(self):
+            return self.title
+
+    # Default and custom manager
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        #* specify app name and url name to which you want to reverse
+        return reverse("blog:post_detail", args=[self.slug])
+
+    def get_comments(self):
+        return self.comments.filter(parent=None).filter(active=True)
+
+#* --- Create model for comments ---
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return self.body
+
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
---
+++ blog/views.py
@@ -0,0 +1,64 @@
+from ast import Try
+from django.shortcuts import render,get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+#* Index Page view to display all Posts in form of a list
+def post_list(request):
+    posts = Post.published.all()
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger: #show always first page
+        posts = paginator.page(1)
+    except EmptyPage: #show last page if out of range
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,'blog/post_list.html',{'posts':posts, page:'pages'})
+
+#* Display specific Post instead all of them
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, status='published')
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            # redirect to same page and focus on that comment
+            return redirect(post.get_absolute_url()+'#'+str(new_comment.id))
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments,'comment_form':comment_form})
+
+# * Create view for replay
+def reply_page(request):
+    if request.method == 'POST':
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            post_id = request.POST.get('post_id')  # from hidden input
+            parent_id = request.POST.get('parent')  # from hidden input
+            post_url = request.POST.get('post_url')  # from hidden input
+            reply = form.save(commit=False)
+
+            reply.post = Post(id=post_id)
+            reply.parent = Comment(id=parent_id)
+            reply.save()
+
+            return redirect(post_url+'#'+str(reply.id))
+
+    return redirect('/')
