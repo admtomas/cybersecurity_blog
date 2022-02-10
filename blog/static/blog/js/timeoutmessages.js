f02e602591467664ef70ae19fa87b091886bcd16
Tomasz Amanowicz <47845850+admtomas@users.noreply.github.com>
2022-02-10 16:34:22 +0000

    initial commit (#1)

---
+++ blog/static/blog/js/timeoutmessages.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // messages timeout for 10 sec
+    setTimeout(function() {
+        $('.message').fadeOut('slow');
+    }, 5000); // <-- time in milliseconds, 1000 =  1 sec
+});
