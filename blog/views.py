bad19ad189b1fdb770a935ecaac85187e6f62271
Tomasz Amanowicz <47845850+admtomas@users.noreply.github.com>
2022-02-20 03:18:11 +0000

    About (#8)
    * about pagae initial draft
    
    * About Page model, views, page refactored to connect to DB and display content propery on template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from ast import Try
 from django.shortcuts import render,get_object_or_404, redirect
-from .models import Post, Comment
+from django.views.generic import TemplateView
+from .models import Post, Comment, About
 from .forms import CommentForm
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
@@ -62,3 +63,9 @@ def reply_page(request):
             return redirect(post_url+'#'+str(reply.id))
 
     return redirect('/')
+
+# * Create view for About PAge
+def about_page(request):
+    about_page = get_object_or_404(About)
+
+    return render(request,'blog/about.html',{'about_page': about_page})
